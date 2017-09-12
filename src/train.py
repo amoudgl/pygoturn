05ebb2fa40d6e14ec086568bb151fe5e0f2a2a18
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-09-12 12:24:48 +0530

    Add basic training script

---
+++ src/train.py
@@ -0,0 +1,113 @@
+# necessary imports
+import datasets
+import model
+import torch
+from torch.autograd import Variable
+from torchvision import transforms
+from helper import ToTensor, show_batch
+from torch.utils.data import DataLoader
+import torch.optim as optim
+
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
+
+def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model = model
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                optimizer = lr_scheduler(optimizer, epoch)
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dset_loaders[phase]:
+                # get the inputs
+                inputs, labels = data
+
+                # wrap them in Variable
+                if use_gpu:
+                    inputs, labels = Variable(inputs.cuda()), \
+                        Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dset_sizes[phase]
+            epoch_acc = running_corrects / dset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model = copy.deepcopy(model)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    return best_model
+
+if __name__ == "__main__":
+    
+    # constants
+    batch_size = 4 
+    n_epochs = 1000
+    lr = 0.001
+
+    # load dataset
+    transform = transforms.Compose([ToTensor()])
+    alov = datasets.ALOVDataset('../data/alov300/imagedata++/',
+                                '../data/alov300/alov300++_rectangleAnnotation_full/',
+                                transform)
+    dataloader = DataLoader(alov, batch_size=batch_size, shuffle=True, num_workers=4)
+    use_gpu = torch.cuda.is_available()
+
+    # load model
+    net = model.GoNet()
+    if use_gpu:
+        net = net.cuda()
+    loss_fn = torch.nn.L1Loss(size_average = False)
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+    net = train_model(net, loss_fn, optimizer, exp_lr_scheduler, num_epochs=n_epochs)
