8a1c27e09ef25419c915d10a9903943e8b308ede
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-09-27 11:41:15 +0530

    Add arguments to training script, restructure code

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 # necessary imports
+import os
 import time
 import copy
 import datasets
@@ -13,48 +14,42 @@ import torch.optim as optim
 import numpy as np
 
 # constants
-batch_size = 1
-n_epochs = 100
-lr = 1e-6
-save_dir = '../saved_checkpoints/exp3/'
 use_gpu = torch.cuda.is_available()
 
+parser = argparse.ArgumentParser(description='GOTURN Training')
+parser.add_argument('-n', '--epochs', default=100, type=int, help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=1, type=int, help='mini-batch size (default: 1)')
+parser.add_argument('-lr', '--learning-rate', default=1e-6, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('-dir', '--save-directory', default='../saved_checkpoints/exp3/', type=str, help='path to save directory')
 
-def evaluate(model, dataloader, criterion, epoch):
-    model.eval()
-    dataset = dataloader.dataset
-    running_loss = 0
-    # test on a sample sequence from training set itself
-    for i in xrange(64):
-        sample = dataset[i]
-        sample['currimg'] = sample['currimg'][None,:,:,:]
-        sample['previmg'] = sample['previmg'][None,:,:,:]
-        x1, x2 = sample['previmg'], sample['currimg']
-        y = sample['currbb']
-        x1 = Variable(x1.cuda())
-        x2 = Variable(x2.cuda())
-        y = Variable(y.cuda(), requires_grad=False)
-        output = model(x1, x2)
-        loss = criterion(output, y)
-        running_loss += loss.data[0]
-        print('[validation] epoch = %d, i = %d, loss = %f' % (epoch, i, loss.data[0]))
+def main():
 
-    seq_loss = running_loss/64
-    return seq_loss
+    args = parser.parse_args()
+    print args
+    # load dataset
+    transform = transforms.Compose([Normalize(), ToTensor()])
+    alov = datasets.ALOVDataset('../data/alov300/imagedata++/',
+                                '../data/alov300/alov300++_rectangleAnnotation_full/',
+                                transform)
+    dataloader = DataLoader(alov, batch_size=args.batch_size, shuffle=True, num_workers=4)
 
-def exp_lr_scheduler(optimizer, epoch, init_lr=lr, lr_decay_epoch=5):
-    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+    # load model
+    net = model.GoNet()
+    loss_fn = torch.nn.L1Loss(size_average = False)
+    if use_gpu:
+        net = net.cuda()
+        loss_fn = loss_fn.cuda()
+    optimizer = optim.SGD(net.classifier.parameters(), lr=args.learning_rate, momentum=args.momentum)
+    if os.path.exists(args.save_directory):
+        print('Directory %s already exists' % (args.save_directory))
+    else:
+        os.makedirs(args.save_directory)
 
-    if epoch % lr_decay_epoch == 0:
-        print('LR is set to {}'.format(lr))
+    # start training
+    net = train_model(net, dataloader, loss_fn, optimizer, args.epochs, args.learning_rate, args.save_directory)
 
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-    return optimizer
-
-def train_model(model, dataloader, criterion, optimizer, lr_scheduler, num_epochs=25):
+def train_model(model, dataloader, criterion, optimizer, num_epochs, lr, save_dir):
     since = time.time()
     dataset_size = dataloader.dataset.len
 
@@ -63,7 +58,7 @@ def train_model(model, dataloader, criterion, optimizer, lr_scheduler, num_epoch
         print('-' * 10)
         model.train()
 
-        optimizer = lr_scheduler(optimizer, epoch)
+        optimizer = exp_lr_scheduler(optimizer, epoch, lr)
         running_loss = 0.0
         i = 0
         # iterate over data
@@ -106,25 +101,39 @@ def train_model(model, dataloader, criterion, optimizer, lr_scheduler, num_epoch
         time_elapsed // 60, time_elapsed % 60))
     return model
 
-def main():
+def evaluate(model, dataloader, criterion, epoch):
+    model.eval()
+    dataset = dataloader.dataset
+    running_loss = 0
+    # test on a sample sequence from training set itself
+    for i in xrange(64):
+        sample = dataset[i]
+        sample['currimg'] = sample['currimg'][None,:,:,:]
+        sample['previmg'] = sample['previmg'][None,:,:,:]
+        x1, x2 = sample['previmg'], sample['currimg']
+        y = sample['currbb']
+        x1 = Variable(x1.cuda())
+        x2 = Variable(x2.cuda())
+        y = Variable(y.cuda(), requires_grad=False)
+        output = model(x1, x2)
+        loss = criterion(output, y)
+        running_loss += loss.data[0]
+        print('[validation] epoch = %d, i = %d, loss = %f' % (epoch, i, loss.data[0]))
 
-    # load dataset
-    transform = transforms.Compose([Normalize(), ToTensor()])
-    alov = datasets.ALOVDataset('../data/alov300/imagedata++/',
-                                '../data/alov300/alov300++_rectangleAnnotation_full/',
-                                transform)
-    dataloader = DataLoader(alov, batch_size=batch_size, shuffle=True, num_workers=4)
+    seq_loss = running_loss/64
+    return seq_loss
 
-    # load model
-    net = model.GoNet()
-    loss_fn = torch.nn.L1Loss(size_average = False)
-    if use_gpu:
-        net = net.cuda()
-        loss_fn = loss_fn.cuda()
-    optimizer = optim.SGD(net.classifier.parameters(), lr=lr, momentum=0.9)
+def exp_lr_scheduler(optimizer, epoch, init_lr, lr_decay_epoch=5):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
 
-    # start training
-    net = train_model(net, dataloader, loss_fn, optimizer, exp_lr_scheduler, num_epochs=n_epochs)
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
 
 if __name__ == "__main__":
     main()
