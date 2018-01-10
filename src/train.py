0612322389dd049ab6addbb37c9d2a0f2a80cccf
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2018-01-10 17:32:56 +0530

    Add multithreading support

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 # necessary imports
-import os
+import os, sys
 import time
 import copy
 import datasets
@@ -14,6 +14,7 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 import numpy as np
 from helper import *
+from multiprocessing.dummy import Pool as ThreadPool
 
 # constants
 use_gpu = torch.cuda.is_available()
@@ -21,6 +22,8 @@ kSaveModel = 20000 # save model after every 20000 steps
 kGeneratedExamplesPerImage = 10; # generate 10 synthetic samples per image in a dataset
 transform = transforms.Compose([Normalize(), ToTensor()])
 
+args = None
+pool = None
 parser = argparse.ArgumentParser(description='GOTURN Training')
 parser.add_argument('-n', '--num-batches', default=500000, type=int, help='number of total batches to run')
 parser.add_argument('-lr', '--learning-rate', default=1e-6, type=float, help='initial learning rate')
@@ -32,10 +35,14 @@ parser.add_argument('-lshift', '--lambda-shift-frac', default=5, type=float, hel
 parser.add_argument('-lscale', '--lambda-scale-frac', default=15, type=float, help='lambda-scale for random cropping')
 parser.add_argument('-minsc', '--min-scale', default=-0.4, type=float, help='min-scale for random cropping')
 parser.add_argument('-maxsc', '--max-scale', default=0.4, type=float, help='max-scale for random cropping')
+parser.add_argument('-workers', '--num-threads', default=10, type=int, help='number of threads for random cropping')
 
 def main():
+
+    global args, pool
     args = parser.parse_args()
-    print args
+    print(args)
+    pool = ThreadPool(args.num_threads)
     # load datasets
     alov = ALOVDataset('../data/alov300/imagedata++/',
                        '../data/alov300/alov300++_rectangleAnnotation_full/',
@@ -68,8 +75,9 @@ def main():
         os.makedirs(args.save_directory)
 
     # start training
-    net = train_model(net, datasets, loss_fn, optimizer, args)
-
+    net = train_model(net, datasets, loss_fn, optimizer)
+    pool.close()
+    pool.join()
 
 def exp_lr_scheduler(optimizer, step, init_lr, gamma, snapshot=50000):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
@@ -82,56 +90,45 @@ def exp_lr_scheduler(optimizer, step, init_lr, gamma, snapshot=50000):
 
     return optimizer, lr
 
+
+def random_crop_tensor(sample):
+    global args
+    prevbb = random_crop(sample,
+                         args.lambda_scale_frac,
+                         args.lambda_shift_frac,
+                         args.min_scale,
+                         args.max_scale)
+    origimg = sample['image']
+    origbb = sample['bb']
+    # Crop previous image with height and width twice the prev bounding box height and width
+    # Scale the cropped image to (227,227,3)
+    crop_curr = transforms.Compose([CropCurr()])
+    scale = Rescale((227,227))
+    transform_prev = transforms.Compose([CropPrev(), scale])
+    prev_img = transform_prev({'image':origimg, 'bb':origbb})['image']
+    # Crop current image with height and width twice the prev bounding box height and width
+    # Scale the cropped image to (227,227,3)
+    curr_obj = crop_curr({'image':origimg, 'prevbb':prevbb, 'currbb':origbb})
+    curr_obj = scale(curr_obj)
+    curr_img = curr_obj['image']
+    currbb = curr_obj['bb']
+    currbb = np.array(currbb)
+    sample = {'previmg': prev_img,
+            'currimg': curr_img,
+            'currbb' : currbb
+            }
+    sample = transform(sample)
+    return sample
+
 # given a dataset sample, generate more samples by synthetic transformations
-def make_training_samples(idx, dataset, args):
+def make_training_samples(idx, dataset):
+    global args, pool
     orig_sample = dataset.get_orig_sample(idx) # unscaled original sample (single image and bb)
-    true_sample = dataset.get_sample(idx) # cropped scaled sample (two frames and bb)
-    true_tensor = transform(true_sample)
+    samples = [orig_sample]*kGeneratedExamplesPerImage
+    results = pool.map(random_crop_tensor, samples)
+    return results
 
-    origimg = orig_sample['image']
-    origbb = orig_sample['bb']
-    x1_batch = torch.Tensor(kGeneratedExamplesPerImage + 1, 3, 227, 227)
-    x2_batch = torch.Tensor(kGeneratedExamplesPerImage + 1, 3, 227, 227)
-    y_batch = torch.Tensor(kGeneratedExamplesPerImage + 1, 4)
-
-    # initialize batch with the true sample
-    x1_batch[0,:,:,:] = true_tensor['previmg']
-    x2_batch[0,:,:,:] = true_tensor['currimg']
-    y_batch[0,:] = true_tensor['currbb']
-
-    for i in range(kGeneratedExamplesPerImage):
-        sample = {'image': origimg, 'bb': origbb}
-        prevbb = random_crop(sample,
-                             args.lambda_scale_frac,
-                             args.lambda_shift_frac,
-                             args.min_scale,
-                             args.max_scale)
-
-        # Crop previous image with height and width twice the prev bounding box height and width
-        # Scale the cropped image to (227,227,3)
-        crop_curr = transforms.Compose([CropCurr()])
-        scale = Rescale((227,227))
-        transform_prev = transforms.Compose([CropPrev(), scale])
-        prev_img = transform_prev({'image':origimg, 'bb':origbb})['image']
-        # Crop current image with height and width twice the prev bounding box height and width
-        # Scale the cropped image to (227,227,3)
-        curr_obj = crop_curr({'image':origimg, 'prevbb':prevbb, 'currbb':origbb})
-        curr_obj = scale(curr_obj)
-        curr_img = curr_obj['image']
-        currbb = curr_obj['bb']
-        currbb = np.array(currbb)
-        sample = {'previmg': prev_img,
-                'currimg': curr_img,
-                'currbb' : currbb
-                }
-        sample = transform(sample)
-        x1_batch[i+1,:,:,:] = sample['previmg']
-        x2_batch[i+1,:,:,:] = sample['currimg']
-        y_batch[i+1,:] = sample['currbb']
-
-    return x1_batch, x2_batch, y_batch
-
-def train_model(model, datasets, criterion, optimizer, args):
+def train_model(model, datasets, criterion, optimizer):
 
     since = time.time()
     curr_loss = 0
@@ -153,7 +150,21 @@ def train_model(model, datasets, criterion, optimizer, args):
             rand_idx = np.random.randint(sz, size=1)[0]
 
             # get training batch by generating new synthetic samples
-            x1, x2, y = make_training_samples(rand_idx, dataset, args)
+            synth_samples = make_training_samples(rand_idx, dataset)
+            # prepare batch
+            true_sample = dataset.get_sample(rand_idx) # cropped scaled sample (two frames and bb)
+            true_tensor = transform(true_sample)
+            x1 = torch.Tensor(kGeneratedExamplesPerImage + 1, 3, 227, 227)
+            x2 = torch.Tensor(kGeneratedExamplesPerImage + 1, 3, 227, 227)
+            y = torch.Tensor(kGeneratedExamplesPerImage + 1, 4)
+            x1[0,:,:,:] = true_tensor['previmg']
+            x2[0,:,:,:] = true_tensor['currimg']
+            y[0,:] = true_tensor['currbb']
+            for k in range(kGeneratedExamplesPerImage):
+                sample = synth_samples[i]
+                x1[k+1,:,:,:] = sample['previmg']
+                x2[k+1,:,:,:] = sample['currimg']
+                y[k+1,:] = sample['currbb']
 
             # wrap them in Variable
             if use_gpu:
@@ -176,6 +187,7 @@ def train_model(model, datasets, criterion, optimizer, args):
             # statistics
             curr_loss = loss.data[0]
             print('[training] step = %d/%d, dataset = %d, loss = %f' % (batch, args.num_batches, i, curr_loss))
+            sys.stdout.flush()
 
 #         val_loss = evaluate(model, dataloader, criterion, epoch)
 #         print('Validation Loss: {:.4f}'.format(val_loss))
