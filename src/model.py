bd39cb9e000217888847280ff2471824312b2a83
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2019-01-28 15:35:00 +0530

    minor style fixes

--- src/model.py
+++ src/model.py
@@ -2,25 +2,26 @@ import torch
 from torchvision import models
 import torch.nn as nn
 
-class GoNet(nn.Module):
-    ''' Neural Network class
-        Two stream model:
-    ________
-   |        | conv layers              Untrained Fully
-   |Previous|------------------>|      Connected Layers
-   | frame  |                   |    ___     ___     ___
-   |________|                   |   |   |   |   |   |   |   fc4
-               Pretrained       |   |   |   |   |   |   |    * (left)
-               CaffeNet         |-->|fc1|-->|fc2|-->|fc3|--> * (top)
-               Convolution      |   |   |   |   |   |   |    * (right)
-               layers           |   |___|   |___|   |___|    * (bottom)
-    ________                    |   (4096)  (4096)  (4096)  (4)
-   |        |                   |
-   | Current|------------------>|
-   | frame  |
-   |________|
 
-    '''
+class GoNet(nn.Module):
+    """ Neural Network class
+        Two stream model:
+        ________
+       |        | conv layers              Untrained Fully
+       |Previous|------------------>|      Connected Layers
+       | frame  |                   |    ___     ___     ___
+       |________|                   |   |   |   |   |   |   |   fc4
+                   Pretrained       |   |   |   |   |   |   |    * (left)
+                   CaffeNet         |-->|fc1|-->|fc2|-->|fc3|--> * (top)
+                   Convolution      |   |   |   |   |   |   |    * (right)
+                   layers           |   |___|   |___|   |___|    * (bottom)
+        ________                    |   (4096)  (4096)  (4096)  (4)
+       |        |                   |
+       | Current|------------------>|
+       | frame  |
+       |________|
+
+    """
     def __init__(self):
         super(GoNet, self).__init__()
         caffenet = models.alexnet(pretrained=True)
@@ -34,7 +35,7 @@ class GoNet(nn.Module):
                 nn.Linear(4096, 4096),
                 nn.ReLU(inplace=True),
                 nn.Dropout(),
-                nn.Linear(4096,4096),
+                nn.Linear(4096, 4096),
                 nn.ReLU(inplace=True),
                 nn.Dropout(),
                 nn.Linear(4096, 4),
@@ -46,11 +47,12 @@ class GoNet(nn.Module):
             # fully connected layers are weight initialized with
             # mean=0 and std=0.005 (in tracker.prototxt) and
             # biases are set to 1
+            # tracker.prototxt link: https://goo.gl/iHGKT5
             if isinstance(m, nn.Linear):
                 m.bias.data.fill_(1)
                 m.weight.data.normal_(0, 0.005)
 
-    # feed forward through the neural net
+    # feed forward through neural net
     def forward(self, x, y):
         x1 = self.convnet(x)
         x1 = x1.view(x.size(0), 256*6*6)
